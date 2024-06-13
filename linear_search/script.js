f9eca35ac6a00cd6dfab1a87dd44b4bb7cc07d0b
Danil <akuskodanil13@gmail.com>
2024-06-13 21:55:52 +0700

    Add file

---
+++ linear_search/script.js
@@ -0,0 +1,32 @@
+function linearSearchOne(array, key) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === key) {
+      return i;
+    }
+  }
+
+  return null;
+}
+
+function linearSearchAll(array, key) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === key) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+let array = [1, 3, 2, 5, 4, 4, 4, 7, 6, 9, 8];
+let key = 4;
+let resultOne = linearSearchOne(array, key);
+let resultAll = linearSearchAll(array, key);
+
+console.log("Массив: " + array);
+console.log("Ключ поиска: " + key);
+
+console.log("Индекс найденного элемента: " + resultOne);
+console.log("Индексы найденных элементов: " + resultAll);
